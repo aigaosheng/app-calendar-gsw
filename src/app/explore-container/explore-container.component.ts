4ea61a044d6dc1df96b39c01563e9bd6260ab06b
gs <goseng123@gmail.com>
2020-08-13 22:58:22 +0800

    schedule ready

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+
 
 @Component({
   selector: 'app-explore-container',
@@ -6,7 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
-  @Input() name: string;
+  @Input() colname: any;
+  @Input() eventme: Array<any>;
 
   constructor() { }
 
--- src/app/schedule/schedule.page.ts
+++ src/app/schedule/schedule.page.ts
@@ -1,6 +1,25 @@
 import { Component } from '@angular/core';
 import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
+import { AngularFireDatabase, AngularFireList  } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+import { stringify } from 'querystring';
+
+class eventMeta {
+  constructor(
+    public username: string,
+    public title: string,
+    public startTime: Date,
+    public endTime: Date,
+    public allDay: Boolean,
+    public desc: string
+    ) {
+
+   }
+}
+
 @Component({
   selector: 'app-notepad',
   templateUrl: 'schedule.page.html',
@@ -8,19 +27,73 @@ import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser'
 })
 export class NotepadPage {
   inAppBrowser: any;
+  eventlist: Array<any> = new Array();
+  colnames:any = {"user":"username","title":"title", "startTime": "start time", "endTime":"end time"};
 
-  constructor() {}
+
+
+  bookRef: AngularFireList<eventMeta>;
+  public books: Observable<eventMeta[]>;
+
+  constructor(private db: AngularFireDatabase) {
+      //username='gsw';
+      //this.bookRef = this.db.list('/eventInfo',ref => ref.orderByChild('username').equalTo('gsw'));
+      this.bookRef = this.db.list('/eventInfoGs');
+      
+      this.books = this.bookRef.snapshotChanges().pipe(
+        map(changes => 
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      ); 
+
+    
+      this.books.forEach(element => {
+        for (let ev of element ) {
+            /*this.qEvent.push(
+              new eventMeta(ev.username,ev.title,
+                ev.startTime,ev.endTime,ev.allDay,ev.desc)
+            );*/
   
+            let vs:Date=new Date(ev.startTime);
+            let ve:Date=new Date(ev.endTime);
+            let vt:string=ev.title;
+            let vd:string=ev.desc;
+            let va:Boolean=ev.allDay;
+  
+            let vs1:Date=new Date(vs.getTime()+2*24*60*60*1000);
+            let ve1:Date=new Date(ve.getTime()+2*24*60*60*1000);
+  
+       
+            //console.log(currentEvent);
+            this.eventlist.push(new eventMeta(
+              'gs',
+              vt,
+              vs1,
+              ve1,
+              va,
+              vd
+              ));
+  
+         // );
+        }
+        
+      });     
+  }
+  
+  /*
   openWebpage(url: string) {
     const options: InAppBrowserOptions = {
       zoom: 'no'
     }
   
     // Opening a URL and returning an InAppBrowserObject
-    const browser = this.inAppBrowser.create(url, '_self', options);
+    //const browser = this.inAppBrowser.create(url, '_self', options);
+    const browser = this.inAppBrowser.create("htttps://www.zaobao.com", '_self', options);
   
    // Inject scripts, css and more with browser.X
   }
+
+*/
 
 }
 
